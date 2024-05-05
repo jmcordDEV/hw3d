c7dc5536a1a87c9e239c63fc5914d47f900843d8
rspninthefuture <155985814+rspninthefuture@users.noreply.github.com>
2024-05-05 17:56:51 -0400

    Add project files.

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,80 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain
+(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+
+{
+	//Declare class name, 'auto' determines the value automatically
+	const auto pClassName = L"hw3d";
+
+	//Register the window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowEx
+	(
+		0,
+		pClassName,
+		L"r5apex.exe",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		640, 480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG      msg;
+	BOOL gResult;
+	
+	while (gResult = GetMessage(&msg, hWnd, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
+	while (true);
+	
+	return 0;
+}
