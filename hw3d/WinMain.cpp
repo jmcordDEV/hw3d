80f9c3dd9c82d8a538a8ab6efc2224b0e53e68a1
jmcordDEV <155985814+jmcordDEV@users.noreply.github.com>
2024-05-07 01:00:16 -0400

    Changed OutputDebugString to OutputDebugStringA to on line 7 in WinMain.cpp to resolve an argument mismatch (C2664)

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,7 +1,11 @@
 #include <Windows.h>
+#include "WindowsMessageMap.h"
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugStringA(mm(msg, wParam, lParam).c_str());
+
 	switch (msg)
 	{
 	case WM_CLOSE:
